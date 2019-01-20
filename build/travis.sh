947295b5333fdad2f24b5f0d4d44e82e77110f50
Andrew Bennet <me@andrewbennet.com>
2019-01-20 15:49:32 +0000

    Move some install steps

--- build/travis.sh
+++ build/travis.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-
 set -e
 
+brew install mint
+mint run yonaskolb/xcodegen
+pod install
 bundle exec fastlane test
 
 # Deploy on master, if we have access to the necessary secure variables
