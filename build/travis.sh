52424e896802f0bb5a82ebf1898c95b7d71dad62
Andrew Bennet <me@andrewbennet.com>
2019-01-20 16:33:07 +0000

    Attempt to parallelise tests

--- build/travis.sh
+++ build/travis.sh
@@ -2,7 +2,13 @@
 set -e
 
 pod install
-bundle exec fastlane test
+
+xcodebuild \
+	-workspace ReadingList.xcworkspace \
+	-scheme ReadingList \
+	-destination "${TEST_SDK}" \
+	clean test | xcpretty
+
 
 # Deploy on master, if we have access to the necessary secure variables
 if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
